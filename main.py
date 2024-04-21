0dd08a943b295136987fee7486dae2d11c5209ee
joshuajonghankim <joshuajonghankim@gmail.com>
2024-04-21 18:38:48 -0400

    add

--- main.py
+++ main.py
@@ -27,15 +27,15 @@ print("My Program")
 print("=" * 50)
 
 a = len(a)
+# print(a)
 
-
+class animal:
+    def __init__(self):
+        name = ""
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-print(a)
